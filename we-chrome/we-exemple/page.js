ce9c61d47e7065e4de52051b6a70a5c082c860e5
deborah-powers <deborah.powers.dev@gmail.com>
2021-04-14 22:27:31 +0200

    maison 04/14 22:25

--- we-chrome/we-exemple/page.js
+++
@@ -1,7 +0,0 @@
-/* pas de alert
-*/
-console.log ('coucou');
-document.head.innerHTML ="";
-document.body.style.border = "5px solid green";
-document.body.style.backgroundColor = "pink";
-document.body.innerHTML = '<h1>nouveau titre !</h1>' + document.body.innerHTML;
