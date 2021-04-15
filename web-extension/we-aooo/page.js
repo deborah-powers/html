ca56b6cf968005c8b6fdbf279a8c55cc14ecb95f
deborah-powers <deborah.powers.dev@gmail.com>
2021-04-15 09:55:39 +0200

    maison 04/15 09:55

---
+++ web-extension/we-aooo/page.js
@@ -0,0 +1,6 @@
+/* pas de alert */
+
+document.head.innerHTML ="";
+console.log ('coucou');
+document.body.innerHTML = document.getElementsByClassName ('userstuff module')[0].innerHTML;
+document.body.innerHTML = '<h1>nouveau titre !</h1>' + document.body.innerHTML;
