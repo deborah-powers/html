8b3f687bbaee5c246662a8aaa7bdf41aad00c699
deborah-p <deborah.powers@cgi.com>
2021-04-19 10:48:09 +0200

    boulot 04/19 10:45

--- web-extension/we-exemple/popup.js
+++ web-extension/we-exemple/popup.js
@@ -8,3 +8,12 @@ function newText(){
 	else document.getElementsByTagName ('p')[0].innerHTML = textA;
 }
 document.getElementsByTagName ('button')[0].addEventListener ('click', newText);
+
+// ouvrir les liens de la popup, en chrome
+function openLink (event){ window.open (event.target.getAttribute ('href')); }
+var linkList = document.getElementsByTagName ('a');
+for (var l=0; l< linkList.length; l++){
+	linkList[l].addEventListener ('click', openLink);
+}
+
+
