d2a14c1773dfc106c235ddeadf5bbc15eea6738a
deborah-p <deborah.powers@cgi.com>
2021-04-20 19:23:12 +0200

    boulot 04/20 soir

--- web-extension/we-ex-toggle/toggle.js
+++ web-extension/we-ex-toggle/toggle.js
@@ -6,43 +6,43 @@ const jsCode = "console.log ('coucou je suis le code')";
 const jsFile = 'page.js';
 const launchExtension = "launch appli";
 const stopExtension = "stop appli";
-const iconLaunch = 'icon.svg';
-const iconStop = 'icon-off.svg';
+const iconLaunch = 'icon-128.png';
+const iconStop = 'icon-128-bis.png';
 
 function initAction (tab){
-	browser.pageAction.setIcon ({tabId: tab.id, path: "icon-128.png"});
-	browser.pageAction.setTitle ({tabId: tab.id, title: launchExtension});
-	browser.pageAction.show (tab.id);
+	chrome.pageAction.setIcon ({tabId: tab.id, path: "icon-128.png"});
+	chrome.pageAction.setTitle ({tabId: tab.id, title: launchExtension});
+	chrome.pageAction.show (tab.id);
 }
 // réinitialiser l'extension chaque fois que l'onglet est rafraîchi
-browser.tabs.onUpdated.addListener (function (id, changeInfo, tab){
+chrome.tabs.onUpdated.addListener (function (id, changeInfo, tab){
 	initAction (tab);
 });
 // à l'ouverture de firefox, initialiser l'extension pour toutes les pages
-var allTabs = browser.tabs.query({});
+var allTabs = chrome.tabs.query({});
 allTabs.then (function (tabs){
 	for (var tab in tabs) initAction (tab);
 });
 function toggleExtension (tab){
 	function gotTitle (title){
 		if (title === launchExtension){
-			browser.pageAction.setIcon ({ tabId: tab.id, path: iconLaunch });
-			browser.pageAction.setTitle ({ tabId: tab.id, title: stopExtension });
-			browser.tabs.insertCSS ({ file: cssFile });
-			browser.tabs.insertCSS ({ file: cssFile2 });
-		//	browser.tabs.insertCSS ({ code: cssCode });
-			browser.tabs.executeScript ({ file: jsFile });
-		//	browser.tabs.executeScript ({ code: jsCode });
+			chrome.pageAction.setIcon ({ tabId: tab.id, path: iconLaunch });
+			chrome.pageAction.setTitle ({ tabId: tab.id, title: stopExtension });
+			chrome.tabs.insertCSS ({ file: cssFile });
+			chrome.tabs.insertCSS ({ file: cssFile2 });
+		//	chrome.tabs.insertCSS ({ code: cssCode });
+			chrome.tabs.executeScript ({ file: jsFile });
+		//	chrome.tabs.executeScript ({ code: jsCode });
 		} else {
-			browser.pageAction.setIcon ({ tabId: tab.id, path: iconStop });
-			browser.pageAction.setTitle ({ tabId: tab.id, title: launchExtension });
-			browser.tabs.removeCSS ({ file: cssFile });
-			browser.tabs.removeCSS ({ file: cssFile2 });
+			chrome.pageAction.setIcon ({ tabId: tab.id, path: iconStop });
+			chrome.pageAction.setTitle ({ tabId: tab.id, title: launchExtension });
+			chrome.tabs.removeCSS ({ file: cssFile });
+			chrome.tabs.removeCSS ({ file: cssFile2 });
 		}
 	}
 
-	var gettingTitle = browser.pageAction.getTitle ({ tabId: tab.id });
+	var gettingTitle = chrome.pageAction.getTitle ({ tabId: tab.id });
 	gettingTitle.then (gotTitle);
 }
 // lancer l'extention quand je clique sur l'icône
-browser.pageAction.onClicked.addListener (toggleExtension);
+chrome.pageAction.onClicked.addListener (toggleExtension);
