0433bd60bdfe225ebb67f82b7be7045e94b5608f
deborah-powers <deborah.powers.dev@gmail.com>
2022-02-15 16:16:43 +0100

    boulot 02/15 16:16

---
+++ exemples/temp.js
@@ -0,0 +1,33 @@
+
+class DragLimit{
+	constructor(){
+		this.minX =0;
+		this.maxX =0;
+		this.minY =0;
+		this.maxY =0;
+	}
+}
+SVGCircleElement.prototype.getDragLimit = function(){
+	var styles = window.getComputedStyle (this, null);
+	var rstr= styles['r'].replace ('px');
+	var rnbr = parseInt (r);
+
+	var limit = new DragLimit();
+
+	size.
+	var r= this.getAttributeNb ('r');
+	if (!r || r == undefined){
+		var styles = window.getComputedStyle (this, null);
+		r= styles['r']
+	}
+	var d= this.getAttributeNb ('cx')
+}
+
+for (var s=0; s< styles.length; s++) if (styles.hasOwnProperty (styles[s])) console.log (styles[s], styles[styles[s]]);
+
+
+
+var circle = svg.getElementsByTagNameNS (svgNs, 'circle')[0];
+
+console.log (rnb);
+
