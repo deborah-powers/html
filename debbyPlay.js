df929dd20426eeba7b2065957146d87c2e60e960
deborah-powers <deborah.powers.dev@gmail.com>
2022-08-07 22:53:14 +0200

    maison 08/07 22:52

--- debbyPlay.js
+++ debbyPlay.js
@@ -1,14 +1,8 @@
 // dépendence: text.js
 var dpVarList =[];
+var bodyTemplate ="";
 
-function listVar(){
-	document.body.innerHTML = document.body.innerHTML.replace ('\n');
-	document.body.innerHTML = document.body.innerHTML.replace ('\t');
-	document.body.innerHTML = document.body.innerHTML.clean();
-	var bodyText = document.body.innerHTML.replace ('))', '((');
-	var bodyList = bodyText.split ('((');
-	for (var v=1; v< bodyList.length; v=v+2) dpVarList.push (bodyList[v]);
-}
+
 Array.prototype.deep = function(){
 	if (this.length >0 && this[0].constructor.name == 'Array'){
 		var degre =1;
@@ -36,8 +30,7 @@ String.prototype.printVarUnique = function (varName, varValue){
 		for (var l= varValue.length -1; l>=0; l--){
 			message = template.printVarUnique (varName, varValue[l]);
 			text = text.insert (message, d);
-		}
-	}
+	}}
 	else if (varValue.constructor.name == 'Object'){
 		var d= this.index ('(('+ varName +'))');
 		var f= this.index ('>',d) +1;
@@ -47,8 +40,7 @@ String.prototype.printVarUnique = function (varName, varValue){
 		for (var l in varValue) if (typeof (varValue[l]) != 'function'){
 			text = text.insert (template, d);
 			text = text.printVarUnique (varName, varValue[l]);
-		}
-	}
+	}}
 	else text = this.replace ('(('+ varName +'))', varValue);
 	return text;
 }
@@ -68,28 +60,56 @@ function setValueFromName (varName, varValue){
 	if (! varName.contain ('.')) this[varName] = varValue;
 	else{
 		var listName = varName.split ('.');
-		// if (listName.length ==2) this[listName[0]][listName[1]] = varValue;
-		console.log (this[listName[0]],);
-		varName = this[listName[0]];
-		for (var w=1; w< listName.length -1; w++) varName = varName[listName[w]];
-		varName = varValue;
-	}
+		if (listName.length ==2) this[listName[0]][listName[1]] = varValue;
+		else if (listName.length ==3) this[listName[0]][listName[1]][listName[2]] = varValue;
+		else if (listName.length ==4) this[listName[0]][listName[1]][listName[2]][listName[3]] = varValue;
+		else if (listName.length ==5) this[listName[0]][listName[1]][listName[2]][listName[3]][listName[4]] = varValue;
+		else if (listName.length ==6) this[listName[0]][listName[1]][listName[2]][listName[3]][listName[4]][listName[5]] = varValue;
+}}
+// gérer les inputs
+HTMLInputElement.prototype.reload = function(){
+	setValueFromName (this.name, this.value);
+	document.body.innerHTML = bodyTemplate;
+	printVarList();
 }
-function printVarList (varList){
-	// for (var n=0; n< document.body.childNodes.length; n++) console.log (n, document.body.childNodes[n].textContent);
-	for (var v=0; v< varList.length; v++){
-		var varValue = getValueFromName (varList[v]);
-		document.body.innerHTML = document.body.innerHTML.printVarUnique (varList[v], varValue);
-	}
-	var inputList = document.getElementsByTagName ('input');
+HTMLTextAreaElement.prototype.reload = function(){
+	setValueFromName (this.name, this.value);
+	document.body.innerHTML = bodyTemplate;
+	printVarList();
+}
+function printInput (type){
+	var inputList = document.getElementsByTagName (type);
 	for (var i=0; i< inputList.length; i++){
 		var varValue = getValueFromName (inputList[i].name);
 		if (varValue){
 			inputList[i].value = varValue;
-			inputList[i].addEventListener ('change', function (event){
-				setValueFromName (event.target.name, event.target.value);
-				console.log (getValueFromName (event.target.name));
-			});
-		}
+			inputList[i].addEventListener ('change', function (event){ event.target.reload(); });
+}}}
+// les conditions
+function printCondition(){
+	if (document.body.innerHTML.contain ('if=')){
+		var condList = document.body.innerHTML.split ('if=');
+		console.log (condList);
+		for (var c=1; c< condList.length; c++)
 	}
+}
+function printVarList(){
+	// for (var n=0; n< document.body.childNodes.length; n++) console.log (n, document.body.childNodes[n].textContent);
+	for (var v=0; v< dpVarList.length; v++){
+		var varValue = getValueFromName (dpVarList[v]);
+		document.body.innerHTML = document.body.innerHTML.printVarUnique (dpVarList[v], varValue);
+	}
+	printInput ('input');
+	printInput ('textarea');
+	printCondition();
+}
+function dpInit(){
+	bodyTemplate = document.body.innerHTML;
+	document.body.innerHTML = document.body.innerHTML.replace ('\n');
+	document.body.innerHTML = document.body.innerHTML.replace ('\t');
+	document.body.innerHTML = document.body.innerHTML.clean();
+	var bodyText = document.body.innerHTML.replace ('))', '((');
+	var bodyList = bodyText.split ('((');
+	for (var v=1; v< bodyList.length; v=v+2) dpVarList.push (bodyList[v]);
+	printVarList();
 }
