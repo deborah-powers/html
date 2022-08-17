fb013d365ebb603e812142e5938d797465c3031f
deborah-powers <deborah.powers.dev@gmail.com>
2022-08-17 16:56:19 +0200

    boulot 08/17 16:56

--- web-extension/we-ex-toggle/page.js
+++
@@ -1,5 +0,0 @@
-/* pas de alert */
-
-document.head.innerHTML ="";
-console.log ('coucou je suis le file');
-document.body.innerHTML = '<h1>nouveau titre !</h1>' + document.body.innerHTML;
--- web-extension/we-logic-immo/action.js
+++
@@ -1,5 +0,0 @@
-/* pas de alert
-*/
-console.log ('coucou');
-document.head.innerHTML ="";
-document.body.innerHTML = document.getElementsByTagName ('main')[0].innerHTML;	// pour wikipédia
