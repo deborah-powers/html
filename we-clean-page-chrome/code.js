fd6503709b27e334933b46a9d96f223d3f5b43bc
deborah-powers <deborah.powers.dev@gmail.com>
2022-08-18 15:25:49 +0200

    boulot 08/18 15:25

--- we-clean-page-chrome/code.js
+++ we-clean-page-chrome/code.js
@@ -1,10 +1,14 @@
-const id = getTabId();
-
-chrome.scripting.executeScript ({
-    target: {tabId: id, allFrames: true},
-    files: ['text.js', 'cleanPage.js'],
-});
-chrome.scripting.insertCSS ({
-    target: {tabId: id, allFrames: true},
-    files: ['structure.css', 'perso.css'],
-});
+chrome.tabs.query ({ currentWindow: true }, function (tabs){
+	for (var t=1; t< tabs.length; t++){
+		chrome.scripting.executeScript ({
+			target: {tabId: tabs[t].id, allFrames: true},
+			files: ['text.js', 'cleanPage.js']
+		});
+		chrome.scripting.insertCSS ({
+			target: {tabId: tabs[t].id, allFrames: true},
+			files: ['structure.css', 'perso.css'],
+		});
+}});
+/*
+Extension manifest must request permission to access this host
+*/
