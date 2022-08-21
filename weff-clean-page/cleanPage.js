928ee0d170a875716ba91012a54b4e80b51c5d95
deborah-powers <deborah.powers.dev@gmail.com>
2022-08-21 14:20:00 +0200

    date +

--- weff-clean-page/cleanPage.js
+++ weff-clean-page/cleanPage.js
@@ -1,14 +1,32 @@
+// dépendance: text.js
+
 var title = document.head.getElementsByTagName ('title')[0].innerHTML;
 document.head.innerHTML = '<title>' + title + '</title>';
 document.body.innerHTML = document.body.innerHTML.cleanHtml();
 
 HTMLElement.prototype.clean = function(){
-	for (var c= this.children.length -1; c>=0; c--) if (this.children[c].tagName != 'svg'){
-		if (! exists (this.children[c].innerHTML) && ! "A IMG BR HR".contain (this.children[c].tagName)) this.removeChild (this.children[c]);
-		else this.children[c].clean();
-	}
+	// rechercher le main
+	if (this.innerHTML.contain ('</main>')){
+		var c=0;
+		while (c< this.children.length){
+			if (this.children[c].tagName == 'MAIN'){
+				this.innerHTML = this.children[c].innerHTML;
+				c= this.children.length +1;
+			}
+			c++;
+	}}
+	// éliminer les blocs inutiles
+	for (var c= this.children.length -1; c>=0; c--){
+		if (this.children[c].tagName == 'SCRIPT' || this.children[c].tagName == 'NOSCRIPT' || this.children[c].tagName == 'FOOTER')
+			this.removeChild (this.children[c]);
+		else if (this.children[c].tagName != 'svg'){
+			if (! exists (this.children[c].innerHTML) && ! "A IMG BR HR".contain (this.children[c].tagName))
+				this.removeChild (this.children[c]);
+			else this.children[c].clean();
+	}}
 	if (this.childNodes.length ==1 && this.children.length ==1 && ! "A svg IMG BR HR".contain (this.children[0].tagName))
 		this.innerHTML = this.children[0].innerHTML;
+	// effacer les attributs
 	for (var a= this.attributes.length -1; a>=0; a--) this.removeAttribute (this.attributes[a].name);
 }
 HTMLAnchorElement.prototype.clean = function(){
@@ -31,19 +49,12 @@ HTMLImageElement.prototype.clean = function(){
 	for (var a= this.attributes.length -1; a>=0; a--) if (this.attributes[a].name != 'src' && this.attributes[a].name != 'alt')
 		this.removeAttribute (this.attributes[a].name);
 }
-for (var c= document.body.children.length -1; c>=0 ; c--){
-	if (document.body.children[c].tagName == 'SCRIPT') document.body.removeChild (document.body.children[c]);
-	else if (document.body.children[c].tagName == 'NOSCRIPT') document.body.removeChild (document.body.children[c]);
-	else if (document.body.children[c].tagName == 'FOOTER') document.body.removeChild (document.body.children[c]);
+function cleanBody(){
+	if (document.body.innerHTML.contain ('</article>')){
+		var text = document.body.innerHTML.sliceWords ('<article>', '</article>');
+		document.body.innerHTML = text;
+	}
 }
 document.body.clean();
 document.body.innerHTML = document.body.innerHTML.cleanHtml();
-
-if (document.body.innerHTML.contain ('</main>')){
-	var text = document.body.innerHTML.sliceWords ('<main>', '</main>');
-	document.body.innerHTML = text;
-}
-if (document.body.innerHTML.contain ('</article>')){
-	var text = document.body.innerHTML.sliceWords ('<article>', '</article>');
-	document.body.innerHTML = text;
-}
+cleanBody();
