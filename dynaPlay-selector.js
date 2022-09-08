31ac7a1eaaa004a5cddcc956d6334e56cf9f5eb8
deborah-powers <deborah.powers.dev@gmail.com>
2022-09-08 16:02:19 +0200

    boulot 09/08 16:02

--- dynaPlay-selector.js
+++ dynaPlay-selector.js
@@ -10,6 +10,8 @@ utilisation:	dpInit();
 
 var myList =[ 'a', 'b', 'c'];
 function myFunc (name){ console.log ('coucou', name); }
+
+https://javascript.info/custom-elements
 */
 const selectorStyle =`
 	dp-selector {
@@ -26,37 +28,42 @@ const selectorStyle =`
 		background-color: var(--bord-color);
 		color: var(--page-color);
 	}`;
+
+function loadSelector (event){
+	console.log (event.currentTarget.selector);
+	event.currentTarget.selector.listName = event.currentTarget.selector.getAttribute ('list');
+	event.currentTarget.selector.choiceName = event.currentTarget.selector.getAttribute ('choice');
+	var title = event.currentTarget.selector.createNode ('h2', 'choisir: '+ getValueFromName (event.currentTarget.selector.choiceName));
+	var paragraph = null;
+	var liste = getValueFromName (event.currentTarget.selector.listName);
+	for (var l=0; l< liste.length; l++){
+		paragraph = event.currentTarget.selector.createNode ('p', liste[l]);
+		paragraph.addEventListener ('click', function (event){
+			event.target.parentElement.getElementsByTagName ('h2')[0].innerHTML = 'choisir: '+ event.target.innerHTML;
+			setValueFromName (event.currentTarget.selector.choiceName, event.target.innerHTML);
+			document.body.innerHTML = bodyTemplate;
+			printAll();
+		});
+	}
+	event.currentTarget.selector.removeAttribute ('choice');
+	event.currentTarget.selector.removeAttribute ('list');
+}
+
 class Selector extends HTMLElement{
 	constructor(){
 		super();
 		this.choiceName ="";
-		this.choice ="";
-		this.listName =[];
-		this.list =[];
+		this.listName ="";
 	}
 	connectedCallback(){
 		// appelée après le constructeur
 		setStyle (selectorStyle);
-		var selector = this;
 		// retarder l'initialisation du sélecteur. nécessaire si j'utilise des données javascript dans mes attributs
-		window.addEventListener ('load', function (event){
-			selector.listName = selector.getAttribute ('list');
-			selector.choiceName = selector.getAttribute ('choice');
-			selector.list = getValueFromName (selector.listName);
-			selector.choice = getValueFromName (selector.choiceName);
-			var title = selector.createNode ('h2', 'choisir: '+ selector.choice);
-			var paragraph = null;
-			for (var l=0; l< selector.list.length; l++){
-				paragraph = selector.createNode ('p', selector.list[l]);
-				paragraph.addEventListener ('click', function (event){
-					event.target.parentElement.getElementsByTagName ('h2')[0].innerHTML = 'choisir: '+ event.target.innerHTML;
-					setValueFromName (selector.choiceName, event.target.innerHTML);
-					document.body.innerHTML = bodyTemplate;
-					printAll();
-				});
-			}
-			selector.removeAttribute ('choice');
-			selector.removeAttribute ('list');
-		});
-}}
+		console.log ('le callback');
+		this.choiceName = getValueFromName (this.getAttribute ('choice'));
+		this.listName = getValueFromName (this.getAttribute ('list'));
+		window.addEventListener ('load', loadSelector);
+		window.selector = this;
+	}
+}
 customElements.define ('dp-selector', Selector);
