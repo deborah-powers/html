7a36cda1eab3a50a897bd83edee0d602e4d117f4
Deborah POWERS <deborah.powers.dev@gmail.com>
2023-09-08 17:33:40 +0200

    Update cube-svg.js

--- cube-svg.js
+++ cube-svg.js
@@ -5,188 +5,161 @@ le cube à des cotés de 100 unités.
 coté = 100
 diag = coté * r(2)
 r(2) = 1.4142135623730951 = Math.sqrt(2)
+
+3,4 ___ 1,2
+|        |
+|        |
+|        |
+5,6 ____ 0
+
+3 _____ 1,2 _____ 4
+|        |        |
+|        |        |
+|        |        |
+5 ______ 0 ______ 6
+
+3 ______ 1
+|        |
+|        |
+|        |
+5,6 ____ 0
+|        |
+|        |
+|        |
+4 ______ 2
+
+*/
+/* variables fixées par l'utilisateur
+les angles ont une valeur comprise entre [0, 60]
 */
 const cote =100;
-const diag = cote * Math.sqrt(2);
-const diagDemi = (diag /2);
-const diagQuart = (diag /4);
 const posX =150;
 const posY =150;
-var animationNbSec =0;
-
-// animation, faire tourner le cube comme un manège
-var pasGauch = (cote - diagDemi) /60;
-var pasDroit = (cote + diagDemi) /60;
-var pasQuartDroit = (cote - diagQuart) /60;
-var pasQuartGauch = (diagDemi - diagQuart) /60;
-// pasDroit = pasDroit.toFixed (2);
+var anglex =60;
+var angley =0;
 
 SVGCircleElement.prototype.setPos = function (posX, posY){
 	this.setAttributeNb ('cx', posX);
 	this.setAttributeNb ('cy', posY);
 }
-SVGCircleElement.prototype.moveX = function (pos){
-	pos = pos + this.getAttributeFloat ('cx');
-	this.setAttributeNb ('cx', pos);
-}
-SVGCircleElement.prototype.moveY = function (pos){
-	pos = pos + this.getAttributeFloat ('cy');
-	this.setAttributeNb ('cy', pos);
-}
-SVGCircleElement.prototype.moveAll = function (posX, posY){
+SVGCircleElement.prototype.move = function (posX, posY){
 	posX = posX + this.getAttributeFloat ('cx');
 	this.setAttributeNb ('cx', posX);
 	posY = posY + this.getAttributeFloat ('cy');
 	this.setAttributeNb ('cy', posY);
 }
-const cubes = document.getElementsByTagName ('circle');
+function setAngle (angle){
+	// vérifier les valeurs des angles
+	if (angle <0) angle =0;
+	else if (angle >60) angle =60;
+	// return angle /60;
+	return angle;
+}
+// vérifier les valeurs des angles
+anglex = setAngle (anglex);
+angley = setAngle (angley);
 
-function animateX(){
-	animationNbSec +=1;
-	if (animationNbSec <61){
-		cubes[3].moveX (pasGauch);
-		cubes[5].moveX (pasGauch);
-		cubes[4].moveX (pasDroit);
-		cubes[6].moveX (pasDroit);
-		requestAnimationFrame (animateX);
-	}
-	else if (animationNbSec <121){
-		cubes[3].moveX (pasDroit);
-		cubes[5].moveX (pasDroit);
-		cubes[4].moveX (pasGauch);
-		cubes[6].moveX (pasGauch);
-		requestAnimationFrame (animateX);
-	}
-	else if (animationNbSec <181){
-		cubes[3].moveX (- pasDroit);
-		cubes[5].moveX (- pasDroit);
-		cubes[4].moveX (- pasGauch);
-		cubes[6].moveX (- pasGauch);
-		requestAnimationFrame (animateX);
-	}
-	else if (animationNbSec <241){
-		cubes[3].moveX (- pasGauch);
-		cubes[5].moveX (- pasGauch);
-		cubes[4].moveX (- pasDroit);
-		cubes[6].moveX (- pasDroit);
-		requestAnimationFrame (animateX);
-	}
-}
-function animateY(){
-	animationNbSec +=1;
-	if (animationNbSec <61){
-		cubes[1].moveY (pasGauch);
-		cubes[5].moveY (pasGauch);
-		cubes[2].moveY (pasDroit);
-		cubes[6].moveY (pasDroit);
-		requestAnimationFrame (animateY);
-	}
-	else if (animationNbSec <121){
-		cubes[1].moveY (pasDroit);
-		cubes[5].moveY (pasDroit);
-		cubes[2].moveY (pasGauch);
-		cubes[6].moveY (pasGauch);
-		requestAnimationFrame (animateY);
-	}
-	else if (animationNbSec <181){
-		cubes[1].moveY (- pasDroit);
-		cubes[5].moveY (- pasDroit);
-		cubes[2].moveY (- pasGauch);
-		cubes[6].moveY (- pasGauch);
-		requestAnimationFrame (animateY);
-	}
-	else if (animationNbSec <241){
-		cubes[1].moveY (- pasGauch);
-		cubes[5].moveY (- pasGauch);
-		cubes[2].moveY (- pasDroit);
-		cubes[6].moveY (- pasDroit);
-		requestAnimationFrame (animateY);
-	}
-}
-function animateXall(){
-	animationNbSec +=1;
-	if (animationNbSec <61){
-		cubes[3].moveAll (pasGauch, pasQuartGauch);
-		cubes[4].moveAll (pasDroit, pasQuartGauch);
-		cubes[5].moveAll (pasGauch, pasQuartDroit);
-		cubes[6].moveAll (pasDroit, pasQuartDroit);
-		cubes[1].moveY (pasGauch);
-		cubes[2].moveY (pasDroit);
-		requestAnimationFrame (animateXall);
-	}
-	else if (animationNbSec <121){
-		cubes[3].moveAll (- pasGauch, - pasQuartGauch);
-		cubes[5].moveAll (- pasGauch, - pasQuartDroit);
-		cubes[4].moveAll (- pasDroit, - pasQuartGauch);
-		cubes[6].moveAll (- pasDroit, - pasQuartDroit);
-		cubes[1].moveY (- pasGauch);
-		cubes[2].moveY (- pasDroit);
-		requestAnimationFrame (animateXall);
-	}
-}
-function animateYall(){
-	animationNbSec +=1;
-	if (animationNbSec <61){
-		cubes[1].moveAll (pasQuartGauch, pasGauch);
-		cubes[5].moveAll (pasQuartDroit, pasGauch);
-		cubes[2].moveAll (pasQuartGauch, pasDroit);
-		cubes[6].moveAll (pasQuartDroit, pasDroit);
-		cubes[3].moveX (pasGauch);
-		cubes[4].moveX (pasDroit);
-		requestAnimationFrame (animateYall);
-	}
-	else if (animationNbSec <121){
-		cubes[1].moveAll (- pasQuartGauch, - pasGauch);
-		cubes[5].moveAll (- pasQuartDroit, - pasGauch);
-		cubes[2].moveAll (- pasQuartGauch, - pasDroit);
-		cubes[6].moveAll (- pasQuartDroit, - pasDroit);
-		cubes[3].moveX (- pasGauch);
-		cubes[4].moveX (- pasDroit);
-		requestAnimationFrame (animateYall);
-	}
-}
-function posDepart (sens, anglex, angley){
-	/* fixer la position de départ
-	sens = x ou y
-	angle = un entier de 0 à 60
+// constantes utiles pour les calculs
+const diagDemi = cote * Math.sqrt(2) /2;
+const diagQuart = (diagDemi /2);
+const pasGauch = (cote - diagDemi) /60;
+const pasDroit = (cote + diagDemi) /60;
+var animationNbSec =0;
+// récupérer les points. cubes[0] est fixe
+const cubes = document.getElementsByTagName ('circle');
+cubes[0].setPos (posX, posY);
+
+function setPosX (anglex, angley){
+	/* fixer la position des points. elle varie entre le carré et les deux faces côte à côte
+	de   cubes[5].setPos (posX - cote, posY);
+	à    cubes[5].setPos (posX - diagDemi, posY);
+	puis cubes[5].setPos (posX - cote + anglex * pasGauch, pas5);
+	angle entre 0 et 60
 	*/
-	if (anglex <0) anglex =0;
-	else if (anglex >60) anglex =60;
-	if (angley <0) angley =0;
-	else if (angley >60) angley =60;
-	anglexGauch = anglex * pasGauch - cote;
-	anglexDroit = anglex * pasDroit - cote;
-	angleyGauch = posX + angley * pasQuartGauch - cote;
-	angleyDroit = posX + angley * pasQuartDroit - diagDemi;
-	cubes[0].setPos (posX, posY);
-	if ('x' === sens){
-		cubes[1].setPos (posX, posY - cote + pasGauch * angley);
-		cubes[2].setPos (posX, posY - cote + pasDroit * angley);
-		cubes[3].setPos (anglexGauch, posY);
-		cubes[4].setPos (anglexDroit, posY + pasQuartDroit * angley);
-		cubes[5].setPos (anglexGauch, posY + pasQuartGauch * angley);
-		cubes[6].setPos (anglexDroit, posY + pasQuartGauch * angley);
-		/*
-		cubes[3].moveAll (pasGauch, pasQuartGauch);
-		cubes[4].moveAll (pasDroit, pasQuartGauch);
-		cubes[5].moveAll (pasGauch, pasQuartDroit);
-		cubes[6].moveAll (pasDroit, pasQuartDroit);
-		*/
+	anglex = setAngle (anglex);
+	angley = setAngle (angley);
+	if (angley > anglex) angley = anglex;
+	const pas3 = posY - cote + angley * (cote - diagQuart) /60;
+	const pas5 = posY + angley * diagQuart /60;
+	cubes[1].setPos (posX, posY - cote + angley * pasGauch);
+	cubes[2].setPos (posX, posY - cote + angley * pasDroit);
+	cubes[3].setPos (posX - cote + anglex * pasGauch, pas3);
+	cubes[4].setPos (posX - cote + anglex * pasDroit, pas3);
+	cubes[5].setPos (posX - cote + anglex * pasGauch, pas5);
+	cubes[6].setPos (posX - cote + anglex * pasDroit, pas5);
+}
+function setPosY (anglex, angley){
+	/* fixer la position des points. elle varie entre le carré et les deux faces supperposées
+	de cubes[1].setPos (posX, posY - cote);
+	à  cubes[1].setPos (posX, posY - diagDemi);
+	angle entre 0 et 60
+	*/
+	anglex = setAngle (anglex);
+	angley = setAngle (angley);
+	if (angley < anglex) anglex = angley;
+	const pas1 = posX + anglex * diagQuart /60;
+	const pas3 = posX - cote + anglex * (cote - diagQuart) /60;
+	cubes[1].setPos (pas1, posY - cote + angley * pasGauch);
+	cubes[2].setPos (pas1, posY - cote + angley * pasDroit);
+	cubes[3].setPos (pas3, posY - cote + angley * pasGauch);
+	cubes[4].setPos (pas3, posY - cote + angley * pasDroit);
+	cubes[5].setPos (posX - cote + anglex * pasGauch, posY);
+	cubes[6].setPos (posX - cote + anglex * pasDroit, posY);
+}
+
+const pasQuartDroit = (cote - diagQuart) /60;
+const pasQuartGauch = (diagDemi - diagQuart) /60;
+
+function moveX(){
+	// mouvement simple, comme un manège
+	animationNbSec +=1;
+	if (animationNbSec <61){
+		cubes[3].move (pasGauch, 0);
+		cubes[4].move (pasDroit, 0);
+		cubes[5].move (pasGauch, 0);
+		cubes[6].move (pasDroit, 0);
+		requestAnimationFrame (moveX);
 	}
-	else if ('y' === sens){
-		cubes[1].setPos (posX, posY + anglexGauch);
-		cubes[2].setPos (posX, posY + anglexDroit);
-		cubes[3].setPos (posX - cote, posY);
-		cubes[4].setPos (posX - cote, posY);
-		cubes[5].setPos (posX - cote, posY + anglexGauch);
-		cubes[6].setPos (posX - cote, posY + anglexDroit);
+	// TODO rajouter les étapes intermédiaires
+	else if (animationNbSec <121){
+		cubes[3].move (- pasGauch, 0);
+		cubes[4].move (- pasDroit, 0);
+		cubes[5].move (- pasGauch, 0);
+		cubes[6].move (- pasDroit, 0);
+		requestAnimationFrame (moveX);
 	}
 }
-function posCube (sens, angle){
-	posDepart (sens, angle);
-	animationNbSec = angle;
-	if (sens ==='x') animateX();
-	else if (sens ==='y') animateY();
-	else animateXall();
+function moveY(){
+	animationNbSec +=1;
+	if (animationNbSec <61){
+		cubes[1].move (0, pasGauch);
+		cubes[2].move (0, pasDroit);
+		cubes[3].move (0, pasGauch);
+		cubes[4].move (0, pasDroit);
+		requestAnimationFrame (moveY);
+	}
+	else if (animationNbSec <121){
+		cubes[1].move (0, - pasGauch);
+		cubes[2].move (0, - pasDroit);
+		cubes[3].move (0, - pasGauch);
+		cubes[4].move (0, - pasDroit);
+		requestAnimationFrame (moveY);
+	}
 }
-posDepart ('x', 60, 60);
+setPosY (0,0);
+moveY();
+// pour plus tard
+var c0x =0;
+var c0y =0;
+var c1x =0;
+var c1y =0;
+var c2x =0;
+var c2y =0;
+var c3x =0;
+var c3y =0;
+var c4x =0;
+var c4y =0;
+var c5x =0;
+var c5y =0;
+var c6x =0;
+var c6y =0;
