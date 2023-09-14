559a388168f42125578c346615463f860d609628
deborah-powers <deborah.powers.dev@gmail.com>
2023-09-14 17:18:45 +0200

    boulot 09/14 17:18

--- cube-svg.js
+++ cube-svg.js
@@ -35,8 +35,13 @@ les angles ont une valeur comprise entre [0, 60]
 const cote =100;
 const posX =150;
 const posY =150;
+<<<<<<< HEAD
 var anglex =60;
 var angley =0;
+=======
+var anglex =30;
+var angley =40;
+>>>>>>> 2b0f2cc (boulot 09/07 17:07)
 
 SVGCircleElement.prototype.setPos = function (posX, posY){
 	this.setAttributeNb ('cx', posX);
@@ -52,8 +57,12 @@ function setAngle (angle){
 	// vérifier les valeurs des angles
 	if (angle <0) angle =0;
 	else if (angle >60) angle =60;
+<<<<<<< HEAD
 	// return angle /60;
 	return angle;
+=======
+	return angle /60;
+>>>>>>> 2b0f2cc (boulot 09/07 17:07)
 }
 // vérifier les valeurs des angles
 anglex = setAngle (anglex);
@@ -62,8 +71,11 @@ angley = setAngle (angley);
 // constantes utiles pour les calculs
 const diagDemi = cote * Math.sqrt(2) /2;
 const diagQuart = (diagDemi /2);
+<<<<<<< HEAD
 const pasGauch = (cote - diagDemi) /60;
 const pasDroit = (cote + diagDemi) /60;
+=======
+>>>>>>> 2b0f2cc (boulot 09/07 17:07)
 var animationNbSec =0;
 // récupérer les points. cubes[0] est fixe
 const cubes = document.getElementsByTagName ('circle');
@@ -73,12 +85,17 @@ function setPosX (anglex, angley){
 	/* fixer la position des points. elle varie entre le carré et les deux faces côte à côte
 	de   cubes[5].setPos (posX - cote, posY);
 	à    cubes[5].setPos (posX - diagDemi, posY);
+<<<<<<< HEAD
 	puis cubes[5].setPos (posX - cote + anglex * pasGauch, pas5);
+=======
+	puis cubes[5].setPos (posX - cote + anglex * (cote - diagDemi), pas5);
+>>>>>>> 2b0f2cc (boulot 09/07 17:07)
 	angle entre 0 et 60
 	*/
 	anglex = setAngle (anglex);
 	angley = setAngle (angley);
 	if (angley > anglex) angley = anglex;
+<<<<<<< HEAD
 	const pas3 = posY - cote + angley * (cote - diagQuart) /60;
 	const pas5 = posY + angley * diagQuart /60;
 	cubes[1].setPos (posX, posY - cote + angley * pasGauch);
@@ -87,6 +104,16 @@ function setPosX (anglex, angley){
 	cubes[4].setPos (posX - cote + anglex * pasDroit, pas3);
 	cubes[5].setPos (posX - cote + anglex * pasGauch, pas5);
 	cubes[6].setPos (posX - cote + anglex * pasDroit, pas5);
+=======
+	const pas3 = posY - cote + angley * (cote - diagQuart);
+	const pas5 = posY + angley * diagQuart;
+	cubes[1].setPos (posX, posY - cote + angley * (cote - diagDemi));
+	cubes[2].setPos (posX, posY + angley * diagDemi);
+	cubes[3].setPos (posX - cote + anglex * (cote - diagDemi), pas3);
+	cubes[4].setPos (posX - cote + anglex * (cote + diagDemi), pas3);
+	cubes[5].setPos (posX - cote + anglex * (cote - diagDemi), pas5);
+	cubes[6].setPos (posX - cote + anglex * (cote + diagDemi), pas5);
+>>>>>>> 2b0f2cc (boulot 09/07 17:07)
 }
 function setPosY (anglex, angley){
 	/* fixer la position des points. elle varie entre le carré et les deux faces supperposées
@@ -97,6 +124,7 @@ function setPosY (anglex, angley){
 	anglex = setAngle (anglex);
 	angley = setAngle (angley);
 	if (angley < anglex) anglex = angley;
+<<<<<<< HEAD
 	const pas1 = posX + anglex * diagQuart /60;
 	const pas3 = posX - cote + anglex * (cote - diagQuart) /60;
 	cubes[1].setPos (pas1, posY - cote + angley * pasGauch);
@@ -163,3 +191,21 @@ var c5x =0;
 var c5y =0;
 var c6x =0;
 var c6y =0;
+=======
+	const pas1 = posX + anglex * diagQuart;
+	const pas3 = posX - cote + anglex * (cote - diagQuart);
+	cubes[1].setPos (pas1, posY - cote + angley * (cote - diagDemi));
+	cubes[2].setPos (pas1, posY - cote + angley * (cote + diagDemi));
+	cubes[3].setPos (pas3, posY - cote + angley * (cote - diagDemi));
+	cubes[4].setPos (pas3, posY - cote + angley * (cote + diagDemi));
+	cubes[5].setPos (posX - cote + anglex * (cote - diagDemi), posY);
+	cubes[6].setPos (posX - cote + anglex * (cote + diagDemi), posY);
+}
+function moveX(){
+	animationNbSec +=1;
+	if (animationNbSec <61){
+	}
+}
+setPosX();
+moveX();
+>>>>>>> 2b0f2cc (boulot 09/07 17:07)
