56ebc9a69d6319eae637b84f93259642a114fa79
deborah-powers <deborah.powers.dev@gmail.com>
2024-02-18 21:44:23 +0100

    maison 02/18 21:44

--- svg/reseau.js
+++ svg/reseau.js
@@ -1,5 +1,7 @@
 /* afficher un réseau de point dans une image svg
-var nomsPoints =[ 'a', 'b', 'c', 'd', 'e' ];
+dépendance: svg.js
+dans mon fichier svg:
+var pointsListe =[ 'a', 'b', 'c', 'd', 'e' ];
 var relationsPoints =[
 	[0],
 	[1,0],
@@ -7,93 +9,182 @@ var relationsPoints =[
 	[0,1,0,0],
 	[0,0,0,1,0]
 ];
-creerGraphe (nomsPoints, relationsPoints);
-racine (2) = 1.414213562
 */
-const svgWidth = svg.getAttributeNb ('width') /10;
-const svgHeight = svg.getAttributeNb ('height') /10;
-var pointsListe =[];
-var freresListe =[];
-
-function creerGraphe (nomsPoints, relationsPoints){
-	var point = randomPoint10();
-	pointsListe.push (point);
-
-	for (var l=1; l< nomsPoints.length; l++){
-		freresListe =[];
-		for (var m=0; m<l; m++) if (relationsPoints[l][m] >0) freresListe.push (m);
-		if (freresListe.length >0){
-			point = randomPointRef (pointsListe[freresListe[0]][0], pointsListe[freresListe[0]][1]);
-			for (var m=0; m< freresListe.length; m++){
-				if (relationsPoints[l][freresListe[m]] >1) dessinerLigne (point[0], point[1], pointsListe[freresListe[m]][0], pointsListe[freresListe[m]][1], true);
-				else dessinerLigne (point[0], point[1], pointsListe[freresListe[m]][0], pointsListe[freresListe[m]][1]);
-		}}
-		else point = randomPoint10();
-		pointsListe.push (point);
-	}
-	for (var l=0; l< nomsPoints.length; l++) dessinerNoeud (nomsPoints[l], pointsListe[l][0], pointsListe[l][1]);
-}
-function pointsListeContien (posX, posY){
-	var poContain = false;
-	for (var p=0; p< pointsListe.length; p++) if (pointsListe[p][0] == posX && pointsListe[p][1] == posY) poContain = true;
-	return poContain;
-}
-function randomNb10(){
+function randomNb(){
 	var pos =0;
 	while (pos <=0 || pos >=10){
-		pos = 10* Math.random();
+		pos =10* Math.random();
 		pos = Math.floor (pos);
 	}
 	return pos;
 }
-function randomPoint10(){
-	var posX = randomNb10();
-	var posY = randomNb10();
-	while (pointsListeContien (posX, posY)){
-		posX = randomNb10();
-		posY = randomNb10();
-	}
-	return [ posX, posY ];
-}
 function randomNbRef (ref){
 	var pos =0;
 	while (pos <=0 || pos >=10 || pos < ref -1 || pos > ref +1){
-		pos = 3* Math.random();
+		pos =3* Math.random();
 		pos = Math.floor (pos);
 		pos -=1;
 		pos += ref;
 	}
 	return pos;
 }
-function randomPointRef (refX, refY){
-	var posX = randomNbRef (refX);
-	var posY = randomNbRef (refY);
-	while (pointsListeContien (posX, posY)){
-		posX = randomNbRef (refX);
-		posY = randomNbRef (refY);
+class Point{
+	constructor (nom){
+		this.nom = nom;
+		this.x =0;
+		this.y =0;
 	}
-	return [posX, posY];
+	randomCoord(){
+		this.x = randomNb();
+		this.y = randomNb();
+	}
+	randomCoordFromRef (pointRef){
+		this.x = randomNbRef (pointRef.x);
+		this.y = randomNbRef (pointRef.y);
+	}
+	ajuster (limitsSvg, limitsGraphe){
+		this.x -= limitsGraphe.xmin;
+		this.x *= limitsSvg.width;
+		this.x /= limitsGraphe.width;
+		this.x += limitsSvg.xmin;
+		this.y -= limitsGraphe.ymin;
+		this.y *= limitsSvg.height;
+		this.y /= limitsGraphe.height;
+		this.y += limitsSvg.ymin;
+	}
+	dessiner(){
+		var newCircle = createShape ('ellipse', svg);
+		newCircle.setAttributeNb ('cx', this.x);
+		newCircle.setAttributeNb ('cy', this.y);
+		var newText = createShape ('text', svg);
+		newText.setAttributeNb ('x', this.x);
+		newText.setAttributeNb ('y', this.y);
+		newText.innerHTML = this.nom;
+	}
 }
-function dessinerLigne (posX, posY, posX2, posY2, double){
-	posX = svgWidth * posX -1;
-	posY = svgHeight * posY -1;
-	posX2 = svgWidth * posX2 -1;
-	posY2 = svgHeight * posY2 -1;
-	var newLine = createShape ('line', svg);
-	if (double) newLine.setAttribute ('class', 'double');
-	newLine.setAttributeNb ('x1', posX);
-	newLine.setAttributeNb ('y1', posY);
-	newLine.setAttributeNb ('x2', posX2);
-	newLine.setAttributeNb ('y2', posY2);
+class Points extends Array{
+	// liste de Point
+	constructor (pointsListe){
+		super();
+		if (pointsListe !== null && pointsListe !== undefined && pointsListe.length >0) this.fromListe (pointsListe);
+	}
+	fromListe (pointsListe){
+		for (var p=0; p< pointsListe.length; p++){
+			var point = new Point (pointsListe[p]);
+			this.push (point);
+	}}
+	contient (point){
+		var listContient = false;
+		var p=0;
+		while (p< this.length && listContient === false){
+			if (this[p].x === point.x && this[p].y === point.y && this[p].nom !== point.nom) listContient = true;
+			p++;
+		}
+		return listContient;
+	}
+	findPoint (nom){
+		// pos = -1 si aucun point ne porte ce nom, ou son idex
+		var pos =-1;
+		var p=0;
+		while (p< this.length && pos <0){
+			if (this[p].nom === nom) pos =p;
+			p++;
+		}
+		return pos;
+	}
+	addCoord (pos){
+		this[pos].randomCoord();
+		while (this.contient (this[pos])) this[pos].randomCoord();
+	}
+	addCoordFromRef (pos, pointRef){
+		this[pos].randomCoordFromRef (pointRef);
+		while (this.contient (this[pos])) this[pos].randomCoordFromRef (pointRef);
+	}
+	addPoint (nom){
+		var pos = this.findPoint (nom);
+		if (pos >=0) return;
+		var point = new Point (nom);
+		point.randomCoord();
+		while (this.contient (point)) point.randomCoord();
+		this.push (point);
+	}
+	addPointFromRef (nom, pointRef){
+		var pos = this.findPoint (nom);
+		if (pos >=0) return;
+		var point = new Point (nom);
+		point.randomCoordFromRef (pointRef);
+		while (this.contient (point)) point.randomCoordFromRef (pointRef);
+		this.push (point);
+	}
+	ajuster (labelWidth, labelHeight){
+		const limitsSvg = new Limits();
+		limitsSvg.fromSvg (labelWidth, labelHeight);
+		const limitsGraphe = new Limits();
+		limitsGraphe.fromPoints (this);
+		for (var p=0; p< this.length; p++) this[p].ajuster (limitsSvg, limitsGraphe);
+	}
+	dessiner(){ for (var p=0; p< this.length; p++) this[p].dessiner(); }
 }
-function dessinerNoeud (letter, posX, posY){
-	posX = svgWidth * posX -1;
-	posY = svgHeight * posY -1;
-	var newCircle = createShape ('circle', svg);
-	newCircle.setAttributeNb ('cx', posX);
-	newCircle.setAttributeNb ('cy', posY);
-	var newText = createShape ('text', svg);
-	newText.setAttributeNb ('x', posX);
-	newText.setAttributeNb ('y', posY);
-	newText.innerHTML = letter;
+class Graphe{
+	constructor (pointsListe, relationsPoints){
+		this.relations = relationsPoints;
+		this.points = new Points (pointsListe);
+	}
+	calculer(){
+		this.points.addCoord (0);
+		for (var p=1; p< this.points.length; p++){ for (var f=0; f< this.relations[p].length; f++){
+			if (this.relations[p][f] >0) this.points.addCoordFromRef (p, this.points[f]);
+			else this.points.addCoord (p);
+	}}}
+	dessiner (labelWidth, labelHeight){
+		this.points.ajuster (labelWidth, labelHeight);
+		// dessiner les lignes
+		for (var p=1; p< this.points.length; p++){ for (var f=0; f< this.relations[p].length; f++){
+			if (this.relations[p][f] >0){
+				var newLine = createShape ('line', svg);
+				if (this.relations[p][f] >1) newLine.setAttribute ('class', 'double');
+				newLine.setAttributeNb ('x1', this.points[p].x);
+				newLine.setAttributeNb ('y1', this.points[p].y);
+				newLine.setAttributeNb ('x2', this.points[f].x);
+				newLine.setAttributeNb ('y2', this.points[f].y);
+		}}}
+		this.points.dessiner();
+	}
+}
+class Limits{
+	constructor(){
+		this.width =0;
+		this.height =0;
+		this.xmin =0;
+		this.ymin =0;
+	}
+	fromPoints (pointsListe){
+		this.xmin = pointsListe[0].x;
+		var xmax = pointsListe[0].x;
+		this.ymin = pointsListe[0].y;
+		var ymax = pointsListe[0].y;
+		for (var p=1; p< pointsListe.length; p++){
+			if (pointsListe[p].x < this.xmin) this.xmin = pointsListe[p].x;
+			else if (pointsListe[p].x > xmax) xmax = pointsListe[p].x;
+			if (pointsListe[p].y < this.ymin) this.ymin = pointsListe[p].y;
+			else if (pointsListe[p].y > ymax) ymax = pointsListe[p].y;
+		}
+		this.width = xmax - this.xmin;
+		this.height = ymax - this.ymin;
+	}
+	fromSvg (labelWidth, labelHeight){
+		const marge =10;
+		var margeX = marge;
+		if (labelWidth !== null && labelWidth !== undefined) margeX += labelWidth;
+		var margeY = marge;
+		if (labelHeight !== null && labelHeight !== undefined) margeY += labelHeight;
+		this.width = svg.getAttributeNb ('width') -2* margeX;
+		this.height = svg.getAttributeNb ('height') -2* margeY;
+		this.xmin = margeX;
+		this.ymin = margeY;
+}}
+function creerGraphe (pointsListe, relationsPoints, labelWidth, labelHeight){
+	var graphe = new Graphe (pointsListe, relationsPoints);
+	graphe.calculer();
+	graphe.dessiner (labelWidth, labelHeight);
 }
