32a4ea6a69621a9e8b68be5bdc62863c6d19c0b4
deborah-powers <deborah.powers.dev@gmail.com>
2024-05-27 16:16:26 +0200

    boulot 05/27 16:16

--- model-accessibilité/action.js
+++
@@ -1,9 +0,0 @@
-angular.module ('accessApp', ['ui.router']);
-// le routage
-angular.module ('accessApp').config (function ($stateProvider, $urlRouterProvider, $locationProvider){
-	$stateProvider.state ({ name: 'navclav', url: '/navclav', templateUrl: 'page-navclav.html' });
-	$stateProvider.state ({ name: 'gnrl', url: '/generalites', templateUrl: 'page-general.html' });
-	$urlRouterProvider.otherwise ('/generalites');
-	$locationProvider.hashPrefix ('');
-//	$locationProvider.html5Mode (true);
-});
